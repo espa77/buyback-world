3a90d8166bdf2664477c84e9c956e6c5b1898df5
espa77 <ethansteiner@gmail.com>
2016-07-13 14:43:21 -0400

    pricing functionality added

--- app/controllers/price.js
+++ app/controllers/price.js
@@ -5,26 +5,49 @@ export default Ember.Controller.extend({
 
     userNotSelectedSize: true,
 
-    uniquePrice: Ember.computed('model', 'selected-device', function(){
-        let modval = this.get('selectedDevice').device_attributes[0];
-        let typeval = this.get('selectedDevice').device_attributes[1];
+    uniqueSize: Ember.computed('model', 'selected-device', function(){
+        let typeval = this.get('selectedDevice').device_attributes[0];
+        let modval = this.get('selectedDevice').device_attributes[1];
         let netval = this.get('selectedDevice').device_attributes[2];
-        var model = this.get('model');
+        let model = this.get('model');
         var deviceSize = [];
-        var filtered = model.filterBy('device_type', modval );
-        var filteredTwice = filtered.filterBy('device_model', typeval);
-        var filteredThrice = filteredTwice.filterBy('network', netval);
-        filteredThrice.forEach(function(size){
+        let filtered = model.filterBy('device_type', typeval )
+            .filterBy('device_model', modval)
+            .filterBy('network', netval);
+        filtered.forEach(function(size){
             if (!deviceSize.isAny('size', size.get('size'))) {
                 deviceSize.pushObject(size);
             }
         });
         return deviceSize;
     }),
+
+    devicePrice: function() {
+        Ember.computed('model', 'selected-device', function () {
+            let userDevice = this.get('selectedDevice').device;
+            let userModel = this.get('selectedDevice').model;
+            let userNetwork = this.get('selectedDevice').network;
+            var userSize = this.get('selectedDevice').size;
+            if (userSize === null) {
+                userSize = "16";
+            }
+            var model = this.get('model');
+            let finalDevice = model.filterBy('device_type', userDevice)
+                .filterBy('device_model', userModel)
+                .filterBy('network', userNetwork)
+                .filterBy('size', parseInt(userSize));
+            return finalDevice;
+        });
+    },
+
     actions: {
         sizeValue(sizeVal) {
             this.get('selectedDevice').addSize(sizeVal);
             this.set('userNotSelectedSize', false);
+            this.devicePrice();
+        },
+        priceValue(priceVal) {
+            console.log(priceVal);
         }
     }
 
