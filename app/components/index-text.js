909073cd5d762977f5825a675b965248e47c647c
espa77 <ethansteiner@gmail.com>
2016-08-23 15:10:17 -0400

    styling and images

--- app/components/index-text.js
+++ app/components/index-text.js
@@ -5,12 +5,12 @@ export default Ember.Component.extend({
     didInsertElement() {
             var tl = new TimelineLite(),
                 text = $("#index-text-one-hour"),
-                split = new SplitText(text, {type:"chars"}),
-                chars = split.chars,
-                centerIndex = Math.floor(chars.length / 2),
+                split = new SplitText(text, {type:"words"}),
+                words = split.words,
+                centerIndex = Math.floor(words.length / 2),
                 i;
-            for (i = 0; i < chars.length; i++) {
-                tl.from(chars[i], 1, {x:(i - centerIndex) * 60, opacity:0, ease:Power2.easeOut}, i * 0.08);
+            for (i = 0; i < words.length; i++) {
+                tl.from(words[i], 1, {x:(i - centerIndex) * 60, opacity:0, ease:Power2.easeOut}, i * 0.08);
             }
             return tl;
     }
--- app/components/sticky-header.js
+++ app/components/sticky-header.js
@@ -3,13 +3,39 @@ import Ember from 'ember';
 const {
     Component,
     inject,
+    observer,
+    get,
+    set,
+    run
 } = Ember;
 
 export default Component.extend({
-    selectedDevice: inject.service('selected-device'),
+    selectedDevice: inject.service(),
+
     tagName: 'section',
+    classNames:['main_h'],
+    classNameBindings: ['sticky'],
+    sticky: false,
+    topPos: 0,
+    offset: 70,
+
+    onWindowScroll(e) {
+        let scroll = $(e.currentTarget).scrollTop(),
+            topPos = (get(this, 'topPos') * -1);
+        if (scroll > topPos) {
+            set(this, 'sticky', true);
+        } else {
+            set(this, 'sticky', false);
+        }
+    },
+
+    modelAdded: observer('selectedDevice.device_attributes.model', function() {
+        $(window).off('scroll', this._windowScroll);
+        set(this, 'sticky', true);
+    }),
 
     didInsertElement() {
+        this._super(...arguments);
         document.querySelector('.material-design-hamburger__icon').addEventListener(
             'click',
             function () {
@@ -27,7 +53,29 @@ export default Component.extend({
                     child.remove('material-design-hamburger__icon--from-arrow');
                     child.add('material-design-hamburger__icon--to-arrow');
                 }
+            }),
+        this._windowScroll = run.bind(this, 'onWindowScroll');
+        $(window).on('scroll', this._windowScroll);
+        set(this, 'topPos', $('.main_h').offset().top);
 
-            });
+        $('.mobile-toggle').click(function () {
+            if ($('.main_h').hasClass('open-nav')) {
+                $('.main_h').removeClass('open-nav');
+            } else {
+                $('.main_h').addClass('open-nav');
+            }
+        });
+
+        $('.main_h li a').click(function () {
+            if ($('.main_h').hasClass('open-nav')) {
+                $('.navigation').removeClass('open-nav');
+                $('.main_h').removeClass('open-nav');
+            }
+        });
+    },
+    willRemoveElement() {
+        this._super(...arguments);
+        $(window).off('scroll', this._windowScroll);
+        this._super(...arguments);
     }
 });
