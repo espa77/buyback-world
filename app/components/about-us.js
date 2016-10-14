0810ce39a488aeb2daa34e6747b19a05adb4adfe
espa77 <ethansteiner@gmail.com>
2016-10-14 13:38:33 -0400

    re-branding and styling changes

--- app/components/about-us.js
+++ app/components/about-us.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    emberChimpResponses: {
+        success: 'Please click the link in the email we just sent you.',
+        error: 'Oops, something went wrong.  Please try again.',
+        invalidError: 'Please enter a valid email address.',
+        attemptsError: 'Please try again in about 5 minutes.'
+    }
 });
--- app/components/index-text.js
+++ app/components/index-text.js
@@ -1,48 +1,47 @@
 import Ember from 'ember';
 
 const {
-    Component,
-    $
+    Component
 } = Ember;
 
 export default Component.extend({
 
 
-    didInsertElement() {
-        var tl = new TimelineLite(),
-            textOne = $("#instant-quote"),
-            textTwo = $("#free-pickup"),
-            textThree = $("#same-day-payment"),
-            splitOne = new SplitText(textOne, {type: "chars"}),
-            splitTwo = new SplitText(textTwo, {type: "chars"}),
-            splitThree = new SplitText(textThree, {type: "chars"}),
-            charsOne = splitOne.chars,
-            charsTwo = splitTwo.chars,
-            charsThree = splitThree.chars,
-            centerIndexOne = Math.floor(charsOne.length / 2),
-            centerIndexTwo = Math.floor(charsTwo.length / 2),
-            centerIndexThree = Math.floor(charsThree.length / 2),
-            i;
-        for (i = 0; i < charsOne.length; i++) {
-            tl.from(charsOne[i], 1, {
-                x: (i - centerIndexOne) * 20,
-                opacity: 0,
-                ease: Power2.easeOut
-            }, i * 0.05);
-        }
-        for (i = 0; i < charsTwo.length; i++) {
-            tl.from(charsTwo[i], 1, {
-                x: (i - centerIndexTwo) * 30,
-                opacity: 0,
-                ease: Power2.easeOut
-            }, i * 0.07, 0.05);
-        }
-        for (i = 0; i < charsThree.length; i++) {
-            tl.from(charsThree[i], 1, {
-                x: (i - centerIndexThree) * 40,
-                opacity: 0,
-                ease: Power2.easeOut
-            }, i * 0.10, 0.12);
-        }
-    }
+    // didInsertElement() {
+    //     var tl = new TimelineLite(),
+    //         textOne = $("#instant-quote"),
+    //         textTwo = $("#free-pickup"),
+    //         textThree = $("#same-day-payment"),
+    //         splitOne = new SplitText(textOne, {type: "chars"}),
+    //         splitTwo = new SplitText(textTwo, {type: "chars"}),
+    //         splitThree = new SplitText(textThree, {type: "chars"}),
+    //         charsOne = splitOne.chars,
+    //         charsTwo = splitTwo.chars,
+    //         charsThree = splitThree.chars,
+    //         centerIndexOne = Math.floor(charsOne.length / 2),
+    //         centerIndexTwo = Math.floor(charsTwo.length / 2),
+    //         centerIndexThree = Math.floor(charsThree.length / 2),
+    //         i;
+    //     for (i = 0; i < charsOne.length; i++) {
+    //         tl.from(charsOne[i], 1, {
+    //             x: (i - centerIndexOne) * 20,
+    //             opacity: 0,
+    //             ease: Power2.easeOut
+    //         }, i * 0.05);
+    //     }
+    //     for (i = 0; i < charsTwo.length; i++) {
+    //         tl.from(charsTwo[i], 1, {
+    //             x: (i - centerIndexTwo) * 30,
+    //             opacity: 0,
+    //             ease: Power2.easeOut
+    //         }, i * 0.07, 0.05);
+    //     }
+    //     for (i = 0; i < charsThree.length; i++) {
+    //         tl.from(charsThree[i], 1, {
+    //             x: (i - centerIndexThree) * 40,
+    //             opacity: 0,
+    //             ease: Power2.easeOut
+    //         }, i * 0.10, 0.12);
+    //     }
+    // }
 });
